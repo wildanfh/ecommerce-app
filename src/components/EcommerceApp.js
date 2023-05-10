9b2d5aa947b8e919bbb1613b1a7c20be681ee1a7
Wildanf <wildanf2005@gmail.com>
2023-05-10 21:28:30 +0700

    add HomePage component

--- src/components/EcommerceApp.js
+++ src/components/EcommerceApp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "../pages/Homepage";
+import HomePage from "../pages/HomePage";
 
 function EcommerceApp() {
   return (
